8f46a815b5f39661be9d4d90402959c75e7eed42
qian1ong <15936882727@139.com>
2016-12-21 15:55:28 +0800

    1

---
+++ files/core.php
@@ -0,0 +1,15 @@
+<?php
+ini_set("display_errors", "On"); error_reporting(E_ALL);
+
+require_once 'tags.php';
+require_once 'conf.php';
+require_once 'markdown.php';
+
+function mc_404()
+{
+  header('HTTP/1.0 404 Not Found');
+  echo "<h1>404 Not Found</h1>";
+  echo "The page that you have requested could not be found.";
+  exit();
+}
+?>
